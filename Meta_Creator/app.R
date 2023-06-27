c1d91c07aac52192f9e277d478f982220b3e57f6
AlexanderHolmes0 <aholme27@vols.utk.edu>
2023-06-27 17:49:48 -0400

    Main Stuffs

---
+++ Meta_Creator/app.R
@@ -0,0 +1,69 @@
+
+library(shiny)
+library(shinythemes)
+
+meta_creator <- function(title, descrip, url, image) {
+    require(shiny)  
+    HTML(paste0(
+        '<!-- HTML Meta Tags -->
+<title>"', title, '"</title>
+<meta name="description" content="', descrip, '">
+
+<!-- Google / Search Engine Tags -->
+<meta itemprop="name" content="', title, '">
+<meta itemprop="description" content="', descrip, '">
+<meta itemprop="image" content="', image, '">
+
+<!-- Google / Search Engine Tags -->
+<meta name="title" content="', title, '">
+<meta name="description" content="', descrip, '">
+<meta name="image" content="', image, '">
+
+<!-- Facebook Meta Tags -->
+<meta property="og:url" content="', url, '">
+<meta property="og:type" content="website">
+<meta property="og:title" content="', title, '">
+<meta property="og:description" content="', descrip, '">
+<meta property="og:image" content="', image, '">
+
+<!-- Twitter Meta Tags -->
+<meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:url" content="', url, '">
+<meta name="twitter:title" content="', title, '">
+<meta name="twitter:description" content="', descrip, '">
+<meta name="twitter:image" content="', image, '">
+'))
+    
+}
+
+ui <- fluidPage(
+    theme = shinytheme('united'),
+    # Application title
+    titlePanel("Metatags_Creator"),
+
+    
+    sidebarLayout(
+        sidebarPanel(
+            textInput("title", label = "Website Title"),
+            textInput("descrip", label = "Website Description"),
+            textInput("url",label = "Website URL"),
+            textInput("image",label = "Image URL")
+        ),
+
+        mainPanel(
+           h1("Quick and Dirty Metatags Output"),
+           verbatimTextOutput('tags')
+        )
+    )
+)
+
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$tags <- renderText({
+        meta_creator(input$title,input$descrip,input$url,input$image)
+})
+}
+# Run the application 
+shinyApp(ui = ui, server = server)
